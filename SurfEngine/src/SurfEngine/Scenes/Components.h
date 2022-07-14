60d4c9137bc9aa7f04b172d2fae5a03ec3065baf
TimJKent <59515387+TimJKent@users.noreply.github.com>
2022-07-13 22:31:43 -0500

    Added Material Properties

--- SurfEngine/src/SurfEngine/Scenes/Components.h
+++ SurfEngine/src/SurfEngine/Scenes/Components.h
@@ -246,6 +246,11 @@ namespace SurfEngine{
 		glm::vec2 Size = { 1.0f, 1.0f };
 		glm::vec2 Offset = { 0.0f,0.0f };
 
+		float Density = 1.0f;
+		float Friction = 0.5f;
+		float Restitution = 0.0f;
+		float RestitutionThreshold = 0.5f;
+
 		BoxColliderComponent() = default;
 		BoxColliderComponent(const BoxColliderComponent&) = default;
 	};
