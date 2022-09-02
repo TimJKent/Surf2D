8dc9e576af9e68c88ad7a3123ff93dcbe12973ad
TimJKent <59515387+TimJKent@users.noreply.github.com>
2022-09-02 12:36:23 -0500

    OpenGLFrameBuffer bug fix

--- SurfEngine/src/SurfEngine/Platform/OpenGl/OpenGLFrameBuffer.cpp
+++ SurfEngine/src/SurfEngine/Platform/OpenGl/OpenGLFrameBuffer.cpp
@@ -76,7 +76,7 @@ namespace SurfEngine {
 			return false;
 		}
 
-		static GLenum HazelFBTextureFormatToGL(FramebufferTextureFormat format)
+		static GLenum FBTextureFormatToGL(FramebufferTextureFormat format)
 		{
 			switch (format)
 			{
@@ -219,7 +219,7 @@ namespace SurfEngine {
 
 		auto& spec = m_ColorAttachmentSpecifications[attachmentIndex];
 		glClearTexImage(m_ColorAttachments[attachmentIndex], 0,
-			Utils::HazelFBTextureFormatToGL(spec.TextureFormat), GL_INT, &value);
+			Utils::FBTextureFormatToGL(spec.TextureFormat), GL_INT, &value);
 	}
 
 }
